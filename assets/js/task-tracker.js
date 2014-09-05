02f701288ec91b5070d97fa15475b375252145fa
jermifer <jennifer.mankin@gmail.com>
2014-09-05 12:46:19 -0400

    initial app creation

---
+++ assets/js/task-tracker.js
@@ -0,0 +1,64 @@
+(function() {
+	var taskTracker = angular.module('TaskTracker', []);
+	
+	taskTracker.controller('NewTask', function() {
+		this.task = {};
+		this.showErrors = false;
+		
+		this.taskAdd = function(valid, tasks) {
+			this.showErrors = true;
+			if( valid ) {
+				tasks.unshift(this.task);
+				this.task = {};
+				this.showErrors = false;
+			}
+		};
+	});
+	
+	taskTracker.controller('TaskListing', ['$http', function($http) {
+		this.tasks = [
+          	{
+        		"name": "Test Task #1",
+        		"date": "12/01/2012",
+        		"assigned": "John Doe"
+        	},
+
+        	{
+        		"name": "Test Task #2",
+        		"date": "12/02/2012",
+        		"assigned": "John Doe"
+        	},
+
+        	{
+        		"name": "Test Task #3",
+        		"date": "12/03/2012",
+        		"assigned": "John Doe"
+        	},
+
+        	{
+        		"name": "Test Task #4",
+        		"date": "12/04/2012",
+        		"assigned": "John Doe"
+        	},
+
+        	{
+        		"name": "Test Task #5",
+        		"date": "12/05/2012",
+        		"assigned": "John Doe"
+        	},
+
+        	{
+        		"name": "Test Task #6",
+        		"date": "12/06/2012",
+        		"assigned": "John Doe"
+        	},
+
+        	{
+        		"name": "Test Task #7",
+        		"date": "12/07/2012",
+        		"assigned": "John Doe"
+        	}
+        ];
+	}]);
+
+})();
